a91919af2c8e8794dde09487cc63956858d643ea
shafrinakterr <shafrinakterr@gmail.com>
2024-01-23 21:27:54 +0600

    movieDetailsModal added

--- src/cine/MovieCard.jsx
+++ src/cine/MovieCard.jsx
@@ -1,35 +1,50 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import { getImgUrl } from "../utils/cine-utility";
 import Rating from "./Rating";
+import MovieDetailsModal from "./MovieDetailsModal";
 
-const MovieCard = ({movie}) => {
-    return (
-        <figure
-        className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl"
-      >
+const MovieCard = ({ movie }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const handleModalClose =()=>{
+    setShowModal(false);
+    selectedMovie(null);
+  }
+  const handleMoveSelection =(movie)=>{
+    setShowModal(true);
+    setSelectedMovie(movie)
+  }
+  return (
+    <>
+      {showModal && (
+        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+      )}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
         <img
           className="w-full object-cover"
           src={getImgUrl(movie.cover)}
           alt=""
         />
-        <figcaption className="pt-4">
-          <h3 className="text-xl mb-1">{movie.title}</h3>
-          <p className="text-[#575A6E] text-sm mb-2">
-            {movie.genre}
-          </p>
-          <div className="flex items-center space-x-1 mb-5">
-            <Rating value={movie.rating} />
-          </div>
-          <a
-            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-            href="#"
-          >
-            <img src="./assets/tag.svg" alt="" />
-            <span>${movie.price} | Add to Cart</span>
-          </a>
-        </figcaption>
+        <a href="#" onClick={()=>handleMoveSelection(movie)}>
+          <figcaption className="pt-4">
+            <h3 className="text-xl mb-1">{movie.title}</h3>
+            <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+            <div className="flex items-center space-x-1 mb-5">
+              <Rating value={movie.rating} />
+            </div>
+            <a
+              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+              href="#"
+            >
+              <img src="./assets/tag.svg" alt="" />
+              <span>${movie.price} | Add to Cart</span>
+            </a>
+          </figcaption>
+        </a>
       </figure>
-    );
+    </>
+  );
 };
 
 export default MovieCard;
